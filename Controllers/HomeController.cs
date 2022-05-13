d2e2522046835465babde37eb7c106bc688b4887
Gerallt Hayes <gerallt.hayes@activequote.com>
2022-05-13 18:49:45 +0100

    Styled basic layout and added vehicle model

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MOTChecker.Models;
+using System.Diagnostics;
+
+namespace MOTChecker.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AboutMe()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public VehicleModel CheckMot(string registration)
+        {
+            VehicleModel model = new VehicleModel();
+            model.Registration = registration;
+
+            return model;
+        }
+
+        [HttpGet]
+        public ActionResult Submit(string registration)
+        {
+            ViewBag.Registration = registration;
+            return View("Index");
+        }
+    }
+}
---
+++ Models/VehicleModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MOTChecker.Models
+{
+    public class VehicleModel
+    {
+        public string Registration { get; set; }
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public DateOnly MotExpiryDate { get; set; }
+
+        public int LastMotMileage { get; set; }
+
+    }
+}
